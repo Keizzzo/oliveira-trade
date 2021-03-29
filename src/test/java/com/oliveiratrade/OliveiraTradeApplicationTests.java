822fac16e17a29e3244fbcb22b11e03b2f5d5fae
KeizoAutbank <keizo.kobayashi@linux.autbank>
2021-03-28 23:07:53 -0300

    primeiro commit oliveira-trade

---
+++ src/test/java/com/oliveiratrade/OliveiraTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveiratrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliveiraTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
